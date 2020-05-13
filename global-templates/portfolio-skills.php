aefff2814b03ef19dc288ca8a77a1f0378263bf8
Tillmann Weimer <tillmann1970@gmail.com>
2020-05-13 19:47:13 +0200

    added animator (css/js) for portfolio skills

--- global-templates/portfolio-skills.php
+++ global-templates/portfolio-skills.php
@@ -1,41 +1,9 @@
 <h1>My Programming skills</h1>
 
-<h3>Laravel:</h3>
-<div class="progress" style="height:40px;">
-  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">65%</div>
-</div>
-<h3>WordPress:</h3>
-<div class="progress" style="height:40px;">
-  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">60%</div>
-</div>
-<h3>HTML:</h3>
-<div class="progress" style="height:40px;">
-  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%</div>
-</div>
-
-<h3>CSS:</h3>
-<div class="progress" style="height:40px;">
-  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 90%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">90%</div>
-</div>
-
-<h3>JavaScript:</h3>
-<div class="progress" style="height:40px;">
-  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 75%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">75%</div>
-</div>
-
-<h3>PHP:</h3>
-<div class="progress" style="height:40px;">
-  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
-</div>
-
-<!-- <h3>Commodore Basic:</h3>
-<div class="progress" style="height:40px;">
-  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%</div>
-</div>
 
 <?php printf(__('My Programming skills', 'portfolio')); ?>
                 <!--The animator-->
-                <!--<div class="container">
+                	<div class="container">
                         <div class="data mt-9 mt-1">
                             <input type="radio" name="data"         value="85"><label>WordPress</label>
                             <input type="radio" name="data"         value="99"><label>i18n</label>
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -21,7 +21,15 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.min.css' );
 		wp_enqueue_style( 'understrap-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
 
-		wp_enqueue_script( 'jquery' );
+		//wp_enqueue_script( 'jquery' );
+
+		// Remove WordPress jQuery
+	wp_deregister_script('jquery');
+	// Add jQuery
+	wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.2.1.min.js', [], '3.3.1', true);
+	// Add popper.js
+	wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', ['jquery'], '1.14.7', true);
+	// Add bootstrap.js
 
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/theme.min.js' );
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
