43bb010edb432032032a9ae9bbdfc0b5d8a6094e
Tillmann Weimer <tillmann1970@gmail.com>
2020-05-15 16:20:55 +0200

    moved <h1>

--- global-templates/portfolio-skills.php
+++ global-templates/portfolio-skills.php
@@ -1,75 +1,66 @@
-<h1><?php _e('My Programming skills', ''); ?></h1>
-
 
                 <!--The animator-->
-                	<div class="container">
-                        <div class="data mt-9 mt-1">
-                            <input type="radio" name="data"         value="<?php the_field('wordpress'); ?>"><label>WordPress</label>
-                            <input type="radio" name="data"         value="<?php the_field('i18n') ;?>"><label>i18n</label>
-                            <input type="radio" name="data"         value="<?php the_field('laravel') ;?>"><label>Laravel</label>
-                            <input type="radio" name="data"         value="<?php the_field('docker') ;?>"><label>Docker</label>
-                            <input type="radio" name="data"         value="<?php the_field('php') ;?>"><label>PHP</label>
-                            <input type="radio" name="data"         value="<?php the_field('javascript') ;?>"><label>JavaScript</label>
-                            <input type="radio" name="data"         value="<?php the_field('html') ;?>"><label>HTML</label>
-                            <input type="radio" name="data"         value="<?php the_field('css') ;?>"><label>CSS</label>
-                            <input type="radio" name="data"         value="<?php the_field('scrum') ;?>"><label>SCRUM</label>
-                            <input type="radio" name="data"         value=""><label><?php _e('Reset Data', 'portfolio')?></label>
+                <div class="container mt-5">
+					<h1 class="text-center"><?php _e('My Programming skills', 'portfolio'); ?></h1>
+					<div class="row justify-content-around">
+                        <div class="data mt-9 mt-1 col-3">
+                            <input type="radio" name="data" value="<?php the_field('wordpress'); ?>"><label>WordPress</label><br>
+                            <input type="radio" name="data" value="<?php the_field('i18n') ;?>"><label>i18n</label><br>
+                            <input type="radio" name="data" value="<?php the_field('laravel') ;?>"><label>Laravel</label><br>
+                            <input type="radio" name="data" value="<?php the_field('docker') ;?>"><label>Docker</label><br>
+                            <input type="radio" name="data" value="<?php the_field('php') ;?>"><label>PHP</label><br>
+                            <input type="radio" name="data" value="<?php the_field('javascript') ;?>"><label>JavaScript</label><br>
+                            <input type="radio" name="data" value="<?php the_field('html') ;?>"><label>HTML</label><br>
+                            <input type="radio" name="data" value="<?php the_field('css') ;?>"><label>CSS</label><br>
+                            <input type="radio" name="data" value="<?php the_field('scrum') ;?>"><label>SCRUM</label><br>
+                            <input type="radio" name="data" value=""><label><?php _e('Reset Data', 'portfolio')?></label>
                         </div>
-                    <div class="datas col-md-9 xs-6">
-                        <div class="column c-3">
+                    	<div class="datas col-3 mt-1 xs-6">
+                        	<div class="column c-3">
+  	                        	<div class="lc mt-4">
+  	                            	<div class="numbers n-100">
+  		                            	<div>0</div>
+  		                            	<div>1</div>
+  		                            	<div>2</div>
+  		                            	<div>3</div>
+  		                            	<div>4</div>
+  		                            	<div>5</div>
+  		                            	<div>6</div>
+  		                            	<div>7</div>
+  		                            	<div>8</div>
+  		                            	<div>9</div>
+  	                            	</div>
 
+                                	<div class="numbers n-10">
+  		                            	<div>0</div>
+  		                            	<div>1</div>
+  		                            	<div>2</div>
+  		                            	<div>3</div>
+  		                            	<div>4</div>
+  		                            	<div>5</div>
+  		                            	<div>6</div>
+  		                            	<div>7</div>
+  		                            	<div>8</div>
+  		                            	<div>9</div>
+  	                            	</div>
 
-  	                        <div class="lc col-md-9 mt-4">
-
-  	                            <div class="numbers n-100">
-  		                            <div>0</div>
-  		                            <div>1</div>
-  		                            <div>2</div>
-  		                            <div>3</div>
-  		                            <div>4</div>
-  		                            <div>5</div>
-  		                            <div>6</div>
-  		                            <div>7</div>
-  		                            <div>8</div>
-  		                            <div>9</div>
-  	                            </div>
-
-                                <div class="numbers n-10">
-  		                            <div>0</div>
-  		                            <div>1</div>
-  		                            <div>2</div>
-  		                            <div>3</div>
-  		                            <div>4</div>
-  		                            <div>5</div>
-  		                            <div>6</div>
-  		                            <div>7</div>
-  		                            <div>8</div>
-  		                            <div>9</div>
-  	                            </div>
-
-                                <div class="numbers n-1">
-  		                            <div>0</div>
-  		                            <div>1</div>
-  		                            <div>2</div>
-  		                            <div>3</div>
-  		                            <div>4</div>
-  		                            <div>5</div>
-  		                            <div>6</div>
-  		                            <div>7</div>
-  		                            <div>8</div>
-  		                            <div>9</div>
-  	                            </div>
-                                  <p style="color:black">&nbsp;%</p>
+                                	<div class="numbers n-1">
+  		                            	<div>0</div>
+  		                            	<div>1</div>
+  		                            	<div>2</div>
+  		                            	<div>3</div>
+  		                            	<div>4</div>
+  		                            	<div>5</div>
+  		                            	<div>6</div>
+  		                            	<div>7</div>
+  		                            	<div>8</div>
+  		                            	<div>9</div>
+  	                            	</div>
+                                  <h3 style="color:black">&nbsp;%</h3>
                             </div>
-
-
                         </div>
-
-
-
                     </div>
-
-
                 </div>
+
 
 
