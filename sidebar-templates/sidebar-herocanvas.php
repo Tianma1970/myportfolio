12caa1da1563c39079dc02891951e2683d4fe714
Tillmann Weimer <tillmann1970@gmail.com>
2020-05-16 13:26:09 +0200

    fixed path

--- sidebar-templates/sidebar-herocanvas.php
+++ sidebar-templates/sidebar-herocanvas.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="jumbotron jumbotron-fluid bg-dark text-white mt-3">
 	<div class="row ml-5">
-		<img src="/wp-content/uploads/2019/04/me.jpg" style="width: 200px; height: 200px; border-radius:10px;">
+		<img src="/myportfolio/wp-content/uploads/2019/04/me.jpg" style="width: 200px; height: 200px; border-radius:10px;">
 		<div class="column ml-5">
 			<h1>
 			Tillmann Weimer
